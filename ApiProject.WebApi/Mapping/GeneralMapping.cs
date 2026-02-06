cb835b466b20f73698b790ecea51a302b8e4fa87
Burak <Burak_ozyurt42@outlook.com>
2026-02-06 21:21:56 +0300

    AutoMapper kullanımı

---
+++ ApiProject.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,29 @@
+﻿using ApiProject.WebApi.Dtos.FeatureDtos;
+using ApiProject.WebApi.Dtos.MessageDtos;
+using ApiProject.WebApi.Entities;
+using AutoMapper;
+
+namespace ApiProject.WebApi.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Feature, ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature, CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature, GetByIdFeatureDto>().ReverseMap();
+            CreateMap<Feature, UpdateFeatureDto>().ReverseMap();
+
+        //AutoMapper'deki ReverseMap() metodu, iki yönlü dönüşüm sağlar. Yani, bir nesneyi diğerine dönüştürdüğünüz gibi, tersini de otomatik olarak yapmanıza olanak tanır.
+        // Özetle:
+        // ✔ Tek satırla çift yönlü mapleme sağlar.
+        // ✔ Kod tekrarını azaltır.
+        // ✔ Kolay kullanım sunar.
+
+            CreateMap<Message, ResultMessageDto>().ReverseMap();
+            CreateMap<Message, CreateMessageDto>().ReverseMap();
+            CreateMap<Message, GetByIdMessageDto>().ReverseMap();
+            CreateMap<Message, UpdateMessageDto>().ReverseMap();
+        }
+    }
+}
--- ApiProject.WebApi/Program.cs
+++ ApiProject.WebApi/Program.cs
@@ -1,10 +1,14 @@
+using System.Reflection;
 using ApiProject.WebApi.Context;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Servisleri ekle
 
 builder.Services.AddDbContext<ApiContext>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer(); 
