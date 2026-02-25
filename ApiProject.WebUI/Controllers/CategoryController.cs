674fc6d408e687bff3052a9decd2d1be6886e2af
Burak <Burak_ozyurt42@outlook.com>
2026-02-25 14:47:41 +0300

    category deneme

--- ApiProject.WebUI/Controllers/CategoryController.cs
+++ ApiProject.WebUI/Controllers/CategoryController.cs
@@ -1,11 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
+using ApiProject.WebUI.Dtos.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ApiProject.WebUI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult CategoryList()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> CategoryList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7123/api/Categories");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto);  //metinden jsona =======> deserialize ise jsondan metine
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7123/api/Categories", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("CategoryList");
+            }
             return View();
         }
     }
