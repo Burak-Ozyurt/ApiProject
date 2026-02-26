6ba7f22eb7a08e0dfee442e71a7ac7bcbfbd868a
Burak <Burak_ozyurt42@outlook.com>
2026-02-26 13:54:47 +0300

    müşteri yorumları bitiiii

---
+++ ApiProject.WebUI/Dtos/TestimonialDtos/GetTestimonialByIdDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProject.WebUI.Dtos.TestimonialDtos
+{
+    public class GetTestimonialByIdDto
+    {
+        public int TestimonialId { get; set; }
+        public string nameSurname { get; set; }
+        public string title { get; set; }
+        public string comment { get; set; }
+        public string imageUrl { get; set; }
+    }
+}
