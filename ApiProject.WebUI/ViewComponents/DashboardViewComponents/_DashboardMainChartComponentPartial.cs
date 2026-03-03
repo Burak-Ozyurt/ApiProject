57e93570bbff7afee847976f6bbb64c574ee801a
Burak <Burak_ozyurt42@outlook.com>
2026-03-03 15:30:06 +0300

    mainchart deneme

---
+++ ApiProject.WebUI/ViewComponents/DashboardViewComponents/_DashboardMainChartComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using ApiProject.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.WebUI.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardMainChartComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var vm = new RevenueChartViewModel
+            {
+                Labels = new List<string> { "Jan", "Feb", "Mar", "Apr", "May", "Jun" },
+                Income = new List<int> { 5, 15, 14, 36, 32, 32 },
+                Expense = new List<int> { 7, 11, 30, 18, 25, 13 }
+            };
+
+            return View(vm);
+        }
+    }
+}
